cefe0545e853a9e6cfd9f9a5eeff3e4cb7b01c1b
AngeloMatos08 <angelomatosn@gmail.com>
2026-07-07 13:06:49 -0300

    remoção do analytics do firebase que não estava sendo utilizado no projeto

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,11 +1,9 @@
 // Import the functions you need from the SDKs you need
 import { getFirestore } from "firebase/firestore";
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
-
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
@@ -20,6 +18,5 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 export const db = getFirestore(app);
 export const auth = getAuth(app);
